badc9893e30b52ca90b33cf566f9f7e82d3ac45c
oreste-abizera <oresteabizera11@gmail.com>
2024-09-08 15:28:48 +0200

    fix: PEP8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 Minimum operations to reach n characters
 """
 
+
 def minOperations(n):
     """
     Calculates the minimum operations to reach n characters
@@ -16,10 +17,10 @@ def minOperations(n):
     # Edge cases [0, 1]
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         if n % divisor == 0:
             n = n // divisor
