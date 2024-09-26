d903dbb9be44ed30df20360fffcd9dc651579af6
oreste-abizera <oresteabizera11@gmail.com>
2024-09-26 14:25:55 +0200

    fix: PEP8 validation

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,13 +3,14 @@
 Module to calculate how much rainwater is retained after it rains.
 """
 
+
 def rain(walls):
     """
     Calculate how many square units of water will be retained after it rains.
-    
+
     Arguments:
     walls -- list of non-negative integers representing wall heights
-    
+
     Returns:
     Integer indicating total amount of rainwater retained.
     """
@@ -20,7 +21,7 @@ def rain(walls):
     n = len(walls)
     prev_wall = walls[0]
     next_wall = 0
-    
+
     for i in range(1, n - 1):
         if walls[i] != 0:
             prev_wall = walls[i]
@@ -28,7 +29,7 @@ def rain(walls):
             if prev_wall == 0:
                 continue
             else:
-                for j in range(i + 1, n-1):
+                for j in range(i + 1, n):
                     if walls[j] != 0:
                         next_wall = walls[j]
                         break
